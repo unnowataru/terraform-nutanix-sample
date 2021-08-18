2c711e96e5ca4ad930f6d792b456275df76a0354
tkaburagi <t.kaburagi@me.com>
2021-08-18 16:20:18 +0900

    bug fix

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@
   variable "prov_vmname_prefix" {}
   variable "prov_num" {}
   variable "prov_subnet_name" {}
-  variable "prov_diskimage_name" {}
   variable "prov_vcpu" {}
   variable "prov_sock" {}
   variable "prov_mem" {}
@@ -26,10 +25,6 @@ data "nutanix_cluster" "cluster" {
   name = var.prov_cluster_name
 }
 
-data "nutanix_image" "ahv_diskimage" {
-  image_name = var.prov_diskimage_name
-}
-
 data "nutanix_subnet" "ahv_network" {
   subnet_name = var.prov_subnet_name
 }
@@ -40,7 +35,7 @@ resource "nutanix_virtual_machine" "nutanix_virtual_machine"{
   name                 = "${var.prov_vmname_prefix}${format("%03d",count.index+1)}"
   description          = "Provisioned by Terraform"
   parent_reference = {
-    "kind" = "project"
+    "kind" = "vm"
     "name" = var.parent_name
     "uuid" = var.parent_uuid
   }
@@ -51,20 +46,5 @@ resource "nutanix_virtual_machine" "nutanix_virtual_machine"{
   # Configure Network   
   nic_list {
     subnet_uuid = data.nutanix_subnet.ahv_network.metadata.uuid
-  }
-
-  # Configure Disk
-  disk_list {
-    data_source_reference = {
-        kind = "image"
-        uuid = data.nutanix_image.ahv_diskimage.metadata.uuid
-      }
-    device_properties {
-      disk_address = {
-        device_index = 0
-        adapter_type = "SCSI"
-      }
-      device_type = "DISK"
-    }
   }
 }
